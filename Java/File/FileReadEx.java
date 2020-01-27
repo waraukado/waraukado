25f3596ee03930f90d7c5563e692f8e64f6e4f33
waraukado <56812453+waraukado@users.noreply.github.com>
2020-01-27 11:42:18 +0900

    Create FileReadEx.java

---
+++ Java/File/FileReadEx.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+ 
+public class FileReadEx {
+    // ファイルを読み込むサンプル
+    public static void main(String[] args) {
+    	// Paths.get() の引数は環境により指定
+        Path path1 = Paths.get("D:/GitHub/waraukado/Java/File/","test1.txt");
+        Charset charset = Charset.forName("MS932");
+
+        try {
+        	List<String> line = Files.readAllLines(path1,charset);
+            for (String s : line) {
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

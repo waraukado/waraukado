41ae4e5e491a8b9dc9adb4812cd2c7d423dd63ef
waraukado <56812453+waraukado@users.noreply.github.com>
2020-09-14 10:14:34 +0900

    Update Dragon.java

--- Java/Kimatsu2020-2k/Dragon.java
+++ Java/Kimatsu2020-2k/Dragon.java
@@ -32,28 +32,7 @@ public class Dragon {
 		if(w.hp <= 0){w.defeat();} // 魔法使いが力尽きたか？判定
 	}
 
-	public void attack(Hero h,Fighter f,Wizard w){
-		int hantei = new Random().nextInt(3); //　攻撃対象をランダムに決定
-		int a_damage = new Random().nextInt(21) + damage; // ダメージ量を決定（基本ダメージ＋0～20）
-		System.out.print("ドラゴンの攻撃　");
-			if(hantei == 0) {
-				//攻撃対象：勇者
-				h.hp -= a_damage;
-				System.out.println("勇者" + h.name + "は" + a_damage + "のダメージを受けた");
-				if(h.hp <= 0){h.defeat();} // 勇者が力尽きたか？判定
-			}else if(hantei==1) {
-				//攻撃対象：戦士
-				f.hp -= a_damage;
-				System.out.println("戦士" + f.name + "は" + a_damage + "のダメージを受けた");
-				if(f.hp <= 0){f.defeat();} // 戦士が力尽きたか？判定
-			}else {
-				//攻撃対象：魔法使い
-				w.hp -= a_damage;
-				System.out.println("魔法使い" + w.name + "は" + a_damage + "のダメージを受けた");
-				if(w.hp <= 0){w.defeat();} // 魔法使いが力尽きたか？判定
-			}
-	}
-
+	
 	//力尽きた
 	public void defeat(){
 		System.out.println("ドラゴンを倒した！！");

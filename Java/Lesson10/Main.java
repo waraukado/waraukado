b811d8199c143256948f9827c8b6689819a788ad
waraukado <56812453+waraukado@users.noreply.github.com>
2020-09-15 12:13:51 +0900

    Update Main.java

--- Java/Lesson10/Main.java
+++ Java/Lesson10/Main.java
@@ -1,5 +1,16 @@
+
 public class Main {
+
 	public static void main(String[] args) {
-  
-  }
+		// TODO 自動生成されたメソッド・スタブ
+		//SuperHero sh = new SuperHero("S勇者");
+		Hero h = new Hero("勇者");
+
+
+		Matango pm = new PoisonMatango('A');
+		//h.flying = true; //飛んでる状態
+		h.attack(pm);
+		pm.attack(h);
+	}
+
 }
